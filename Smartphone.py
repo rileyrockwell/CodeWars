4ceb676b3cccce55e7a25f6920f4d00f624b02a0
riley <riley.rockwell@gmail.com>
2022-12-11 16:42:58 -0800

    updates

--- Smartphone.py
+++ Smartphone.py
@@ -15,12 +15,12 @@ def update_inventory(cur_stock, new_stock):
             elif i[1] == j[1]:
                 binary_array.append(1)
 
-        # print(binary_array)
+        print(binary_array)
 
         if sum(binary_array) == 0:
             unique_cur_stock.append(i)
 
-    # print(unique_cur_stock)
+    print(unique_cur_stock)
 
     # parent: new_stock
     # child: cur_stock
@@ -34,12 +34,12 @@ def update_inventory(cur_stock, new_stock):
             elif i[1] == j[1]:
                 binary_array.append(1)
 
-        # print(binary_array)
+        print(binary_array)
 
         if sum(binary_array) == 0:
             unique_new_stock.append(i)
 
-    # print(unique_new_stock)
+    print(unique_new_stock)
 
     # parent: cur_stock
     # child: new_stock
@@ -54,13 +54,13 @@ def update_inventory(cur_stock, new_stock):
                 binary_array.append(1)
                 cum_total = i[0] + j[0]
 
-        # print(binary_array)
+        print(binary_array)
 
         if sum(binary_array) == 1:
             element = cum_total, i[1]
             unique_matches.append(element)
 
-    # print(unique_matches)
+    print(unique_matches)
 
     # combine all 3 lists, and sort alphabetically
     updated_list = unique_cur_stock + unique_new_stock + unique_matches
