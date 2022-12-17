0f88f541a667d4f53f220452f38f73ac310b7020
riley <riley.rockwell@gmail.com>
2022-12-17 03:31:30 -0800

    updates

---
+++ CodeWarsStyleRankingSystem.py
@@ -0,0 +1,9 @@
+# TODO: create the User class
+# it must support rank, progress, and the inc_progress(rank) method
+
+class User(object):
+    def __init__(self, data_member_1, data_member_2):
+        self.data_member_1 = data_member_1
+        self.data_member_2 = data_member_2
+
+    
--- Decomposition1.py
+++ Decomposition1.py
@@ -1,53 +1,58 @@
 # Collaborators: Peter Domitrovich
 import time
 
+
 def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial(n - 1)
+
+
 def is_prime(n):
     b = True
-    for i in range(2, n):
-        if n % i == 0:
+    for index in range(2, n):
+        if n % index == 0:
             b = False
             break
     return b
+
+
 def prime_list(n):
     # generate a list of all the primes of n
-    # by the "sqrt prime factorization theorem"
-    a = int(n**(1/2))
-    b = []
-    for i in range(2, a + 1):
+    a = []
+    for i in range(2, n + 1):
         if is_prime(i) is True:
-            b.append(i)
-    return b
+            if n % i == 0:
+                a.append(i)
+    return a
 
-def decomp(n):
+
+def decomposition(n):
     x = factorial(n)
 
     # find the prime factorization for x
     a = []
     y = x
-    for i in prime_list(x):
-        # by the "sqrt prime factorization theorem"
-        if i > int(x**(1/2)):
-            return a
+    for index in prime_list(x):
         count = 0
         while y > 0:
-            if y % i == 0:
+            if y % index == 0:
                 count += 1
-                y = y // i
+                y = y // index
             else:
                 break
 
         if count > 0:
-            a.append(str(i) + "^" + str(count))
+            a.append(str(index) + "^" + str(count))
 
     return a
 
+
 if __name__ == "__main__":
-    
+    print(prime_list(factorial(7)))
+    print(prime_list(factorial(8)))
+    print(prime_list(factorial(9)))
     '''
     a = time.time()
     print(decomp(12))
